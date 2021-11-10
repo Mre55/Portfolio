d339c65ec6b6ab4065116c529888ec122006a925
mre55 <misganawmihreteab>
2021-11-10 03:26:24 +0300

    Add information of the project to an array

--- JS/script.js
+++ JS/script.js
@@ -7,17 +7,49 @@ const mobile_menu = document.querySelector('.mobile-menu');
 
 const projectDetails = document.querySelectorAll('.see-project-btn')
 const projectDetail = document.querySelector('.project-detail');
+const projectDetailClose = document.querySelector('.close-button-project');
 
 
 const projects = [
     {
         id: 1,
         name:  "Tonic",
-        descrition: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-        image: "./Images/work-one-img.jpg",
-        imageGit: "./Images/detailProjectGit.png",
-        imageLive: "./Images/detailProjectLive.png"
-
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        featured_image: "./Images/work-one-img.jpg",
+        link_to_live: ["See live", "./Images/detailProjectLive.png"],
+        link_to_source: ["See source", "./Images/detailProjectGit.png"],
+        technologies: ["html", "css", "javaScript"],
+        name_detail: ["CANOPY", "Back End Dev", "2015"]
+    },
+    {
+        id: 2,
+        name:  "Multi-Post Stories",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        featured_image: "./Images/work-two-img.jpg",
+        link_to_live: ["See live", "./Images/detailProjectLive.png"],
+        link_to_source: ["See source", "./Images/detailProjectGit.png"],
+        technologies: ["html", "css", "javaScript"],
+        name_detail: ["CANOPY", "Back End Dev", "2015"]
+    },
+    {
+        id: 3,
+        name:  "Tonic",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        featured_image: "./Images/work-three-img.jpg",
+        link_to_live: ["See live", "./Images/detailProjectLive.png"],
+        link_to_source: ["See source", "./Images/detailProjectGit.png"],
+        technologies: ["html", "css", "javaScript"],
+        name_detail: ["CANOPY", "Back End Dev", "2015"]
+    },
+    {
+        id: 4,
+        name:  "Multi-Post Stories",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+        featured_image: "./Images/work-four-img.jpg",
+        link_to_live: ["See live", "./Images/detailProjectLive.png"],
+        link_to_source: ["See source", "./Images/detailProjectGit.png"],
+        technologies: ["html", "css", "javaScript"],
+        name_detail: ["CANOPY", "Back End Dev", "2015"]
     }
 ]
 
@@ -70,43 +102,42 @@ projectDetails.forEach((button)=>{
         projectDetail.classList.add('active');
         projectDetail.innerHTML = `
         <article class="work-container flex project-1">
-            <div class="mobile-menu-header">
+            <div class="project-detail-header">
               <h3>${selected_project.name}</h3>
               <button data-close-button class="close-button-project">&times;</button>
            </div>
            <div class="client-des">
               <ul class="client-name">
-                <li>CANOPY</li>
+                <li>${selected_project.name_detail[0]}</li>
               </ul>
               <ul>
-                <li>Back End Dev</li>
+                <li>${selected_project.name_detail[1]}</li>
               </ul>
               <ul>
-                <li>2015</li>
+                <li>${selected_project.name_detail[2]}</li>
               </ul>
             </div>
           <div class="work-img">
-            <img src=${selected_project.image} alt="Picture for the project Tonic, showing a laptop and a widescreen." />
+            <img src=${selected_project.featured_image} alt="Picture for the project Tonic, showing a laptop and a widescreen." />
           </div>
-          <div class="work-description">
+          <div class="work-description-project">
             <p>
-              ${selected_project.descrition}
+              ${selected_project.description}
             </p>
             <ul class="buttons">
-              <li><button type="button">html</button></li>
-              <li><button type="button">css</button></li>
-              <li><button type="button">javaScript</button></li>
+              <li><button type="button">${selected_project.technologies[0]}</button></li>
+              <li><button type="button">${selected_project.technologies[1]}</button></li>
+              <li><button type="button">${selected_project.technologies[2]}</button></li>
             </ul>
             <div class="project-detail-buttons">
-                <button class="see-project-btn see-source">See Project
-                <img class="see-source-img" src=${selected_project.imageLive} alt="" />
-                </button>
-                <button class="see-project-btn see-source">See Project
-                <img class="see-source-img" src=${selected_project.imageGit} alt="" />
-                </button>
+                <a href="https://mre55.github.io/Portfolio/"><button class="see-project-btn see-source">${selected_project.link_to_live[0]}
+                  <img class="see-source-img" src=${selected_project.link_to_live[1]} alt="" /></button>
+                </a>
+                <a href="https://github.com/Mre55/Portfolio"><button class="see-project-btn see-source">${selected_project.link_to_source[0]}
+                  <img class="see-source-img" src=${selected_project.link_to_source[1]} alt="" /></button>
+                </a>
             </div>
           </div>
-        </article>
-        `;
+        </article>`;
     })
 })
